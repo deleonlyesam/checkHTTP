0d1408fb872686ba979767c076a16a9f6543832b
unknown <ldeleon@FSITL004.flexisourceit.local>
2015-02-03 14:19:23 +0800

    Initial commit for first attempt to do descriptors. To Do: - Improve Try Catch - Logging - Fetch data from YAML

---
+++ checkhttp.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+import logging
+import logging.config
+import requests
+import argparse
+
+import parse_config 
+
+
+class CheckHTTP(object):
+	"""
+		Main class to check http status based from request presented
+		on YAML config config_file
+
+	"""
+	config_file = parse_config.ParseYAML( )
+
+	def __init__(self, config_file): 
+		self.config_file = config_file
+		self.contents = parse_config.ParseContent(self.config_file)
+
+	def request(self):
+
+		url = self.contents.get_url_params("url")
+		method = self.contents.get_url_params("method")
+		follow_redirects = self.contents.get_url_params("follow_redirects")
+		timeout = self.contents.get_url_params("timeout")
+		
+		if method.upper() != "GET": 
+			raise Exception("Only accepts GET as method")
+
+		# Try catch must be immplemented here
+		return requests.get(url,  
+					allow_redirects=follow_redirects,
+					timeout=timeout)
+
+
+if __name__ == '__main__':
+	cmd = argparse.ArgumentParser()
+	cmd.add_argument("--config", help="Accepts YAML file as parameter")
+
+	config_file_path = cmd.parse_args().config
+
+	a = CheckHTTP(config_file_path)
+	print a.request()
---
+++ parse_config.py
@@ -0,0 +1,65 @@
+import os, sys
+import  uuid
+
+class ParseYAML(object):
+	""" 
+	This class is responsible to parse data based from 
+		YAML as input.
+
+	"""
+	def __set__(self, instance, filepath):
+		try: 
+			with open(filepath, 'r') as contents:
+				self.contents = contents.read()
+		except Exception, err:
+			print err
+
+	def __get__(self, instance, owner):
+		import yaml
+		try:
+			self.contents = yaml.load(self.contents)
+			return dict(self.contents)
+		except Exception, err:
+			print err
+
+class ParseContent(ParseYAML):
+	"""
+		Gathers data based from key value pair on the YAML file.
+		Im thinking some revisions here to allow fetching the data
+		in a more simple, dynamic as possible. Eliminate static data
+	"""
+	def __init__(self, contents):
+		super(ParseContent, self).__init__()
+		self.contents = contents
+		self.root = dict(self.contents['task'])
+		self.definition_tree = dict(self.root["definition"])
+		self.run_tree = dict(self.root["run"])
+
+	def get_id(self, key="definition_id"):
+		if key == "definition_id": return self.get_definition("id")
+		if key == "run_id": 
+			return uuid.uuid4().get_hex()
+
+	def get_definition(self, key):
+		"""Gets all key value pairs from definition tree"""
+		try:
+			for each in self.definition_tree:
+				self.definition_tree[key]
+		except KeyError, err:
+			raise Exception("{0} can't be recognized as a key element. \
+				Info: {1}".format(key, err))
+			
+	def get_url_params(self, key):
+		try:
+			parameters = self.definition_tree["parameters"]
+			return parameters[key]
+		except KeyError, err:
+			raise Exception("{0} can't be recognized as a key element. \
+				Info: {1}".format(key, err))
+
+		
+	def get_run(self, key):
+		"""Gets all key value pairs from run tree"""
+		if key == "run_id": return uuid.uuid4().get_hex()
+		for each in self.run_tree:
+			if key == "logging": return self.run_tree[key]["config"]
